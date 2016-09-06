2551d2e6da5d59dfa7087c0cd2704d57784152a1
Joshua Swiss <swissjoshua@gmail.com>
2016-09-06 15:11:48 +0300

    added ngInfiniteScroll to load AJAX data on scroll

--- gec-node-ona/frontend/feed/FeedController.js
+++ gec-node-ona/frontend/feed/FeedController.js
@@ -4,19 +4,28 @@ angular
     
     var self = this;
     self.all = [];
+    self.all.limit = 5;
 
       function FeedGrabber() {
       $http
         .get("https://itunes.apple.com/search?term=hip+hop&country=us")
         .then(function(response) {
           self.all = response.data;
-          self.collection = [].concat(self.all);
-          // console.log(self.collection);
-          console.log(self.all);
+          console.log(self);
           // console.log($scope);
+
         })
     }
     FeedGrabber();
+
+    $scope.loadMore = function() {
+      var incremented = self.all.limit + 5;
+      console.log('load')
+      self.limit = incremented > self.all.results.length ? self.all.results.length : incremented;
+    }
+
+
+
 
 
   }])
