c9c3fb910ba954c52da118c80db541677dd625d4
Joshua Swiss <swissjoshua@gmail.com>
2016-09-09 17:22:02 +0300

    fixed port issue for DB

--- gec-node-ona/api/config/database.js
+++ gec-node-ona/api/config/database.js
@@ -2,6 +2,6 @@
 
 module.exports = {
 
-  "url" : 'mongodb://josh:chewel07@ec2-54-175-179-121.compute-1.amazonaws.com/3000/dummyDB'
+  "url" : 'mongodb://josh:chewel07@ec2-54-175-179-121.compute-1.amazonaws.com/27017/dummyDB'
 
 };
