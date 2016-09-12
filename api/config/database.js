e53f0738a5b56208005d35f43153533f0708cd3a
Joshua Swiss <swissjoshua@gmail.com>
2016-09-12 17:57:29 +0300

    env var set for mLab

--- api/config/database.js
+++ api/config/database.js
@@ -4,5 +4,6 @@ module.exports = {
 
   // "url" : 'mongodb://josh:chewel07@ec2-54-175-179-121.compute-1.amazonaws.com:27017/dummyDB'
   "url" : 'mongodb://localhost:27017/gec'
+  "url" : process.env.MONGOLAB_URI
 
 };
