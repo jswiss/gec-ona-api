dcb949b48e6c7a26608fa08ef0ed983c9a698b2f
Joshua Swiss <swissjoshua@gmail.com>
2016-09-12 18:02:26 +0300

    added dotenv and config

--- api/config/database.js
+++ api/config/database.js
@@ -3,7 +3,7 @@
 module.exports = {
 
   // "url" : 'mongodb://josh:chewel07@ec2-54-175-179-121.compute-1.amazonaws.com:27017/dummyDB'
-  "url" : 'mongodb://localhost:27017/gec'
+  // "url" : 'mongodb://localhost:27017/gec'
   "url" : process.env.MONGOLAB_URI
 
 };
--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express    = require('express');
+const dotenv     = require('dotenv').config();
 const app        = express();
 const server     = require('http').createServer(app);
 const cors       = require('cors');
